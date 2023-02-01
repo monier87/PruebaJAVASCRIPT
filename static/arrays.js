5bdfa9cdcb2c1e3ad3ad6ad75d8e96cf569f2e07
Carlos David <carlos.monier87@gmail.com>
2023-01-31 23:33:34 -0600

    Arreglos

---
+++ static/arrays.js
@@ -0,0 +1,12 @@
+/*Frutas = ["Mango", "Pera", "Manzana", 5, 8, "Isela"];
+document.write(Frutas[5])*/
+
+// Arrays Asociativo
+
+let PC = {
+    Nombre: "David-PC",
+    Procesador: "Intel Core i-3",
+    Ram: "4gb",
+    Espacio: "1TB",
+}
+document.write(Frutas[5])
--- static/codigo.js
+++ static/codigo.js
@@ -1,24 +1,23 @@
-
 /*let nombre= prompt("Agrega tu nombre");
 
 alert=("hola" + nombre)*/
 
 
- //Operdaores de Calculo
+//Operdaores de Calculo
 
- /*let numero = 10;
- numero **= 6
- document.write(numero)*/
+/*let numero = 10;
+numero **= 6
+document.write(numero)*/
 
-  
- //concatenacion
+
+//concatenacion
 
 /*nombre= "Carlos";
   /*frase= " " + nombre + " estoy caminando";
   frase="soy ${nombre} y estoy caminando";
 
   document.write(frase);*/
-  
+
 /*Operadores aritmeticos
 
 let numero = 23;
@@ -29,7 +28,7 @@ let texto2 = "texto 1";
 document.write(numero==numero2)
 document.write(texto == texto2)
 document.write(numero<=numero2)
-*/ 
+*/
 
 /* Condicionales
 
@@ -49,74 +48,56 @@ dineroCofla = prompt("Cuanto dinero Tienes Cofla ");
 dineroRoberto = prompt("Cuanto dinero Tienes Roberto ");
 dineroPedro = prompt("Cuanto dinero Tienes Pedro ");
 
+dineroCofla = parseInt(dineroCofla);
+
 if (dineroCofla >= 0.6 && dineroCofla < 1) {
-    alert("Comprate el helado de mango")
-}
-else if (dineroCofla >= 1 && dineroCofla < 1.6) {
+    alert("Comprate el helado de mango");
+    alert("Y te sobran" + (dineroCofla - 0.6));
+} else if (dineroCofla >= 1 && dineroCofla < 1.6) {
     alert("Comprate el helado de crema")
-}
-else if (dineroCofla >= 1.6 && dineroCofla < 1.7) {
+} else if (dineroCofla >= 1.6 && dineroCofla < 1.7) {
     alert("Comprate el helado de Vainilla")
-}
-else if (dineroCofla >= 1.7 && dineroCofla < 1.8) {
+} else if (dineroCofla >= 1.7 && dineroCofla < 1.8) {
     alert("Comprate el helado de fresa")
-}
-else if (dineroCofla >= 1.8 && dineroCofla < 2.9) {
+} else if (dineroCofla >= 1.8 && dineroCofla < 2.9) {
     alert("Comprate el helado de Mantecado")
-}
-else if (dineroCofla >= 2.9) {
+} else if (dineroCofla >= 2.9) {
     alert("Comprate el helado de Confites o el pote de 1.4kg");
-}else{
+} else {
     alert("Lo sentimos, no puedes comprar nada")
 }
 
 //Roberto
-if (dineroRoberto>= 0.6 && dineroRoberto < 1) {
+if (dineroRoberto >= 0.6 && dineroRoberto < 1) {
     alert("Comprate el helado de mango")
-}
-else if (dineroRoberto >= 1 && dineroRoberto < 1.6) {
+} else if (dineroRoberto >= 1 && dineroRoberto < 1.6) {
     alert("Comprate el helado de crema")
-}
-else if (dineroRoberto >= 1.6 && dineroRoberto < 1.7) {
+} else if (dineroRoberto >= 1.6 && dineroRoberto < 1.7) {
     alert("Comprate el helado de Vainilla")
-}
-else if (dineroRoberto >= 1.7 && dineroRoberto < 1.8) {
+} else if (dineroRoberto >= 1.7 && dineroRoberto < 1.8) {
     alert("Comprate el helado de fresa")
-}
-else if (dineroRoberto >= 1.8 && dineroRoberto < 2.9) {
+} else if (dineroRoberto >= 1.8 && dineroRoberto < 2.9) {
     alert("Comprate el helado de Mantecado")
-}
-else if (dineroRoberto >= 2.9) {
+} else if (dineroRoberto >= 2.9) {
     alert("Comprate el helado de Confites o el pote de 1.4kg");
-}else{
+} else {
     alert("Lo sentimos, no puedes comprar nada")
 }
 
 //Pedro
 
-if (dineroPedro>= 0.6 && dineroPedro < 1) {
+if (dineroPedro >= 0.6 && dineroPedro < 1) {
     alert("Comprate el helado de mango")
-}
-else if (dineroPedro >= 1 && dineroPedro< 1.6) {
+} else if (dineroPedro >= 1 && dineroPedro < 1.6) {
     alert("Comprate el helado de crema")
-}
-else if (dineroPedro >= 1.6 && dineroPedro < 1.7) {
+} else if (dineroPedro >= 1.6 && dineroPedro < 1.7) {
     alert("Comprate el helado de Vainilla")
-}
-else if (dineroPedro>= 1.7 && dineroPedro < 1.8) {
+} else if (dineroPedro >= 1.7 && dineroPedro < 1.8) {
     alert("Comprate el helado de fresa")
-}
-else if (dineroPedro>= 1.8 && dineroPedro < 2.9) {
+} else if (dineroPedro >= 1.8 && dineroPedro < 2.9) {
     alert("Comprate el helado de Mantecado")
-}
-else if (dineroPedro >= 2.9) {
+} else if (dineroPedro >= 2.9) {
     alert("Comprate el helado de Confites o el pote de 1.4kg");
-}else{
+} else {
     alert("Lo sentimos, no puedes comprar nada")
 }
-
-
-
-
-
-
