4498172af3685b239e838355db1bb33f263d14e3
Carlos David <carlos.monier87@gmail.com>
2023-02-05 21:51:29 -0600

    Add Ejercicios de LLAVAES

---
+++ static/ejemplo_PYTHON.js
@@ -0,0 +1,21 @@
+// EJEMPLO PYTHON
+
+/*let nota_alumno = prompt("Nota del alumno");
+if (nota_alumno < 5) {
+    alert("Insuficiente");
+} else if (nota_alumno < 6) {
+    alert("Suficiente");
+} else if (nota_alumno < 7) {
+    alert("Bien");
+} else {
+    alert("Sobresaliente")
+}*/
+
+// Ejercicio EDAD
+
+let edad = prompt("Introduzca su edad");
+if (0 < edad < 100) {
+    alert("La edad es correcta")
+} else {
+    alert("La edad insertada es incorrecta")
+}
